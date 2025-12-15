fe7463813833297f9e9da984b719863414bd8ebb
toukoms <tokiniaina090605@gmail.com>
2025-12-15 12:59:11 +0300

    chore: initialize FastAPI application with Inngest integration

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from rag-sample!")
+import logging
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+import inngest
+import uuid
+import datetime
+from inngest.experimental import ai
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+inngest_client = inngest.Inngest(
+  app_id="rag_app",
+  logger=logging.getLogger("uvicorn"),
+  isProduction=os.getenv("ENV") == "production",
+  serializer=inngest.PydanticSerializer()
+)
+
+app = FastAPI()
+
+inngest.fastapi.setup(app, inngest_client, [])
+
